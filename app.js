4c17ed7488f5165f62fcc8df26ed812d62cacc9d
vodka <kaihui.zhang@yahoo.com>
2020-05-12 07:39:44 +0000

    Version 2 with all modules. Maybe wrong

--- app.js
+++ app.js
@@ -1,38 +1,98 @@
-const express = require("express");
-const app = express();
-const bodyParser = require('body-parser');
+const express    = require("express"),
+	  app        = express(),
+	  bodyParser = require('body-parser'),
+	  mongoose   = require('mongoose');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.set("view engine", "ejs");
+mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true, useUnifiedTopology: true});
 
-let campgrounds = [
-		{name: "Salmon Creek", image: "https://upload.wikimedia.org/wikipedia/commons/2/27/ZTZ-99A_MBT_20170716.jpg"},
-		{name: "Big Deer Lake", image: "https://www.photosforclass.com/download/pixabay-1626412?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2F57e6d7454e53ae14f6da8c7dda793f7f1636dfe2564c704c7d2772d49145c358_1280.jpg&user=272447"},
-		{name: "Axe Head Lake", image: "https://www.photosforclass.com/download/pixabay-3779280?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2F55e7d24a485aac14f6da8c7dda793f7f1636dfe2564c704c7d2772d49145c358_1280.jpg&user=272447"}
-	];
+// check if mongodb is connected
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("we are connected to db;")
+});
 
+// schema setup
+let campgroundSchema = new mongoose.Schema({
+	name: String,
+	image: String,
+	description: String
+});
+
+let Campground = mongoose.model("Campground", campgroundSchema);
+
+// add one campground
+// Campground.create({
+// 	name: "ZTQ-15",
+// 	image: "https://www.military-today.com/tanks/ztq_15_l1.jpg",
+// 	description: "New light tank"
+// }, (err, newCampground) => {
+// 	if (err) {
+// 		console.log("Error when adding to db");
+// 	} else {
+// 		console.log("Added to db");
+// 		console.log(newCampground);
+// 	}
+// })
+
+
+
+// RESTful Routes
 app.get("/", (req, res) => {
 	res.render("landing");
 });
 
+// INDEX - show all campgrounds
 app.get("/campgrounds", (req, res) => {
-
-	
-	res.render("campgrounds", {campgrounds: campgrounds});	
+	Campground.find({}, function(err, allCampgrounds) {
+		if (err) {
+			console.log("Error :0");
+		} else {
+			res.render("index", {campgrounds: allCampgrounds});	
+		}
+	})
 });
 
+// CREATE - create a new campground
 app.post("/campgrounds", (req, res) => {
 	// get info from the req
 	let name = req.body.name;
 	let url = req.body.image;
-	let newCampground = {name: name, image: url};
-	campgrounds.push(newCampground);
+	let description = req.body.description;
+	Campground.create({
+		name: name,
+		image: url,
+		description: description
+	}, function(err, newCampground) {
+		if (err) {
+			console.log("Error when adding newCampground");
+		} else {
+			console.log("Just added a newCampground");
+			console.log(newCampground);
+		}
+	})
 	// redirect
 	res.redirect("/campgrounds");
 });
 
+// NEW - show the form to create a new campground
 app.get("/campgrounds/new", (req, res) => {
 	res.render("new");	
+})
+
+// SHOW - show more info about one campground
+app.get("/campgrounds/:id", (req, res) => {
+	// find the campground with provided ID
+	let id = req.params.id; // this id is from the url
+	Campground.findById(id, function(err, foundCampground) {
+		if (err) {
+			console.log(err);
+		} else {
+			res.render("show", {campground: foundCampground});
+		}
+	});
 })
 
 app.listen(process.env.PORT || 3000, process.env.IP, function() {
